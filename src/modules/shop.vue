db01c2bc53743b08dabbf351ac64eb1954dafae8
林伟伟 <linweiwei@xiaoyouzi.com>
2018-05-09 19:52:29 +0800

    addshop

---
+++ src/modules/shop.vue
@@ -0,0 +1,27 @@
+<template>
+     <div class="store-profile">
+
+     </div>
+     <div class="order-section">
+        <div class="menu-bar">
+
+        </div>
+        <div class="food-list">
+
+        </div>
+     </div>
+</template>
+
+<script>
+    export default {
+        name: "shop"
+    }
+</script>
+
+<style scoped>
+
+   .store-profile {
+
+   }
+
+</style>
